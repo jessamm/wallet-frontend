f545496e873fe4f65b58917b13aea66f72231f41
Victor Pineda <vpinedaa@unah.hn>
2021-06-27 21:53:05 -0600

    Temporal

--- FrontEnd/src/screens/Login.js
+++ FrontEnd/src/screens/Login.js
@@ -6,42 +6,6 @@ import google from '../assets/img/google.svg';
 const Login = () => {
     return (
         <div className="login-page" style={{height: '100vh'}} >
-            
-            <section id="topbar" class="d-flex align-items-center">
-                <div class="container d-flex justify-content-center justify-content-md-between">
-                    <div class="contact-info d-flex align-items-center">
-                        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contacto@ejemplo.com</a></i>
-                        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+504 3153-9251</span></i>
-                    </div>
-                    <div class="social-links d-none d-md-flex align-items-center">
-                        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                    </div>
-                </div>  
-            </section>
-
-            <header id="header" class="d-flex align-items-center">
-                <div class="container d-flex justify-content-between align-items-center">
-
-                <div class="logo">
-                    <h1><a href="index.html">Wallet</a></h1>
-                </div>
-
-                <nav id="navbar" class="navbar">
-                    <ul>
-                        <li><a class="active" href="index.html">Inicio</a></li>
-                        <li><a href="about.html">Informacion</a></li>
-                        <li><a href="services.html">Servicios</a></li>
-                        <li><a href="team.html">Equipo</a></li>
-                        <li><a href="contact.html">Contactanos</a></li>
-                    </ul>
-                    <i class="bi bi-list mobile-nav-toggle"></i>
-                </nav>
-                </div>
-            </header>
-
             <section className="section section-shaped section-lg">
                 <div className="shape shape-style-1 bg-gradient-default">
                     <span />
