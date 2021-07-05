9acda421cb862b564de867d763c4e48d25b3d5f0
Victor Pineda <vpinedaa@unah.hn>
2021-07-05 16:53:05 -0600

    Landing

--- FrontEnd/src/screens/LandingPage.js
+++ FrontEnd/src/screens/LandingPage.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../styles/global.css"
 
 const LandingPage = () => {
     return (
         <div className="landing-page" >
             <div className="wrapper">
+
+                <section className="d-flex align-items-center top-bar">
+                    <div className="container d-flex justify-content-center justify-content-md-between">
+                        
+                        <div className="contact-info d-flex align-items-center">
+                            <i className="bi bi-envelope d-flex align-items-center">
+                                <Link to="#" className="correoTop">Wallet@gmail.com
+                                </Link>
+                            </i>
+                            <i className="bi bi-phone d-flex align-items-center ms-4">
+                                <span className="telefonoTop">+504 1111-2222</span>
+                            </i>
+                        </div>
+
+                        <div className="social-links d-none d-md-flex align-items-center">
+                            <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
+                            <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
+                            <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
+                        </div>
+                    </div>
+                </section>
+                
                 <div className="section section-hero section-shaped">
                     <div className="shape shape-style-3 shape-default">
                         <span className="span-150" />
