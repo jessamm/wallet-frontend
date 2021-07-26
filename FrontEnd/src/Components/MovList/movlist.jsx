767ed4cece13d1148b527f0e6ef1f5907f1ca105
RAMROG <86012753+RAMROG@users.noreply.github.com>
2021-07-25 22:49:03 -0600

    Update movlist.jsx
    Se hizo el metodo para mostrar los movimientos recientes

--- FrontEnd/src/Components/MovList/movlist.jsx
+++ FrontEnd/src/Components/MovList/movlist.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -7,19 +7,10 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from '../Title/title';
+import { Component } from 'react';
 
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-
-const rows = [
-  createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
-  createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
-  createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
-];
+const API = process.env.REACT_APP_API;
+const idUsuario = localStorage.getItem("idUsuario");
 
 function preventDefault(event) {
   event.preventDefault();
@@ -31,32 +22,49 @@ const UseStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function movlist() {
+export default function Movlist() {
   const classes = UseStyles();
+  const url='https://jsonplaceholder.typicode.com/users'
+  const [data,setData]=useState([])
+
+//Funcion para llamar la data de ultimos movimientos
+  const movimientosRecientes=async()=>{
+    const response=await fetch(url)
+    console.log(response.status)
+    const responseJSON=await response.json()
+    setTodos(responseJSON)
+    console.log(responseJSON)
+  }
+
+  useEffect(() => {
+    movimientosRecientes()  
+  }, []);
+
   return (
     <React.Fragment>
-      <Title>Recent Orders</Title>
+      <Title align="center">Movimientos recientes</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>#</TableCell>
+            <TableCell>nombre</TableCell>
+            <TableCell>tipo</TableCell>
+            <TableCell>monto</TableCell>
+            <TableCell align="right">Fecha</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
+          {todos.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.username}</TableCell>
+              <TableCell>{item.username}</TableCell>
+              <TableCell align="right">{item.email}</TableCell>
             </TableRow>
           ))}
         </TableBody>
+
       </Table>
       <div className={classes.seeMore}>
         <Link color="primary" href="#" onClick={preventDefault}>
